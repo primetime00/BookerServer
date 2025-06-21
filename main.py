d24eb52b8f5656b4fd295d77f2913509c5eaebc3
Ryan Kegel <ryan.kegel@gmail.com>
2025-06-21 00:25:30 -0400

    Trying out a backup route

--- main.py
+++ main.py
@@ -2,28 +2,30 @@ from werkzeug.wrappers import Request, Response
 import json, os, hashlib, re, time
 from flask import Flask, request, jsonify, send_file, abort
 from mutagen.mp3 import MP3
+from tinytag import TinyTag
 app = Flask(__name__)
 
 
 def calculateId(directory):
-    dirs = [x for x in os.listdir(directory) if os.path.isfile(directory + '/' +x) and os.path.splitext(directory + '/' +x)[1].lower() == '.mp3']
+    dirs = [x for x in os.listdir(directory) if os.path.isfile(directory + '/' +x) and (os.path.splitext(directory + '/' +x)[1].lower() == '.mp3' or os.path.splitext(directory + '/' +x)[1].lower() == '.m4b')]
     if not dirs:
         return 0
     f1 = directory + '/' + dirs[0]
-    with open(f1, 'rb') as mp3File:
+    with open(f1, 'rb') as audioFile:
         # read contents of the file
-        data = mp3File.read()
+        data = audioFile.read()
         # pipe contents of the file through
         hex = hashlib.md5(data).hexdigest()
         return hex
 
 def getChapters(directory):
-    dirs = [x for x in os.listdir(directory) if os.path.isfile(directory + '/' +x) and os.path.splitext(directory + '/' +x)[1].lower() == '.mp3']
+    dirs = [x for x in os.listdir(directory) if os.path.isfile(directory + '/' +x) and (os.path.splitext(directory + '/' +x)[1].lower() == '.mp3' or os.path.splitext(directory + '/' +x)[1].lower() == '.m4b')]
     if not dirs:
         return None
     return sorted(dirs)
 
-def formToJson(form):
+def formToJson2(form):
+    form = form.replace(", Book 2", "")
     article = re.sub(r'(\w+)=>', r'"\1": ', form)
     ce = re.compile(r':\s*([\w\s]+)')
     matches = ce.finditer(article)
@@ -53,15 +55,59 @@ def formToJson(form):
     else:
         return form
 
+def formToJson(form):
+    article = form[1:-1]
+    keys = re.findall("\w+=>", article)
+    if not keys:
+        if not str(form).startswith('{'):
+            return form
+        return json.loads(form)
+    vals = re.split('\w+=>', article.strip())[1:]
+    for i in range(0, len(vals)):
+        vals[i] = vals[i].rstrip()
+        if vals[i].endswith(','):
+            vals[i] = vals[i][0:-1]
+        if vals[i].startswith('['):
+            values = vals[i][1:-1].split(',')
+            vArray = []
+            for v in values:
+                try:
+                    float(v)
+                    vArray.append(int(v))
+                except:
+                    vArray.append('"'+v+'"')
+            vals[i] = vArray #'['+','.join(vArray)+']'
+        elif vals[i] == 'true':
+            vals[i] = '"True"'
+        elif vals[i] == 'false':
+            vals[i] = '"False"'
+        else:
+            try:
+                float(vals[i])
+                vals[i] = int(vals[i])
+            except ValueError:
+                vals[i] = '"'+vals[i]+'"'
+
+
+
+    for i in range(0, len(keys)):
+        keys[i] = keys[i].rstrip()
+        if keys[i].endswith('=>'):
+            keys[i] = keys[i][0:-2]
+
+    m = {}
+    for i in range(0, len(keys)):
+        m[keys[i]] = vals[i]
+    return m
 
 def getDuration(bookDir, chapters):
     totalLen = 0
     lenList = []
     for item in chapters:
         path = bookDir + '/' + item
-        audio = MP3(path)
-        totalLen += audio.info.length
-        lenList.append(int(audio.info.length))
+        audio = TinyTag.get(path)
+        totalLen += audio.duration
+        lenList.append(int(audio.duration))
     return totalLen, lenList
 
 
@@ -96,7 +142,7 @@ def update():
             chapters = getChapters(bookDir)
             totalDuration, lenList = getDuration(bookDir, chapters)
             item = {"directory":  bookName, "position":  0, "chapter": 0, "crc":  id, "title": bookName,
-                    'chapters': chapters, 'type': 'mp3', 'complete': False, 'duration': int(totalDuration),
+                    'chapters': chapters, 'type': 'm4a', 'complete': False, 'duration': int(totalDuration),
                     'chapterDurations': lenList}
             data.append(item)
             item['lastWriteDevice'] = 'PHONE'
@@ -151,6 +197,32 @@ def calculatePosition(currentChapter, currentPosition, allDurations):
     pos += currentPosition
     return pos
 
+@app.route('/backup', methods=['POST'])
+def backup():
+    # Parse JSON array from request
+    content = request.get_json()
+    if not content or 'data' not in content:
+        return jsonify({'error': 'Missing data'}), 400
+
+    updates = content['data']
+
+    # Load existing books data
+    with open("books/books.json", "rt") as f:
+        books = json.load(f)
+
+    # Update books with incoming backup data
+    for update in updates:
+        for book in books:
+            if book['crc'] == update.get('crc'):
+                book['position'] = update.get('position', book['position'])
+                book['chapter'] = update.get('chapter', book['chapter'])
+                break
+
+    # Save updated books data
+    with open("books/books.json", "wt") as f:
+        json.dump(books, f, indent=4)
+
+    return jsonify({'status': 'backup complete'})
 
 @app.route('/progress', methods=['POST'])
 def progress():
@@ -186,7 +258,7 @@ def progress():
             item['lastReadTime'] = now
         else: #device is a watch
             if progressItem["chapter"] == item["chapter"] and progressItem["position"] == item["position"]: #nothing has changed at all
-                print("No change detected")
+                print("No changes detected")
                 progressItem['update'] = False
             else:
                 if lastWriteTime > lastReadTime and lastWriteDevice == 'PHONE':
@@ -261,7 +333,10 @@ def checkin(): #done from the phone
     return jsonify({'time': str(now)})
 
 
+#form = "{chapters=>[-2030043135, -2030043134, -2030043133, -2030043132, -2030043131, -2030043130, -2030043129, -2030043128, -2030043127, -2030043126, -2030043125, -2030043124, -2030043123, -2030043122, -2030043121, -2030043120, -2030043119, -2030043118, -2030043117, -2030043116, -2030043115, -2030043114, -2030043113, -2030043112, -2030043111, -2030043110, -2030043109, -2030043108, -2030043107, -2030043106, -2030043105, -2030043104, -2030043103, -2030043102], title=>Galactic Breach Ruins of the Galaxy, Book 2, chapterDurations=>[1449, 1414, 1608, 1453, 1371, 1388, 1869, 568, 914, 1097, 976, 840, 880, 945, 759, 1221, 960, 1120, 819, 876, 878, 775, 826, 861, 1371, 1406, 990, 1104, 1416, 905, 1031, 66, 393, 1409], position=>20, chapter=>0}"
+#g = formToJson2(form)
+#g = 5
 
 if __name__ == '__main__':
     from werkzeug.serving import run_simple
-    run_simple('0.0.0.0', 8080, app)
+    run_simple('0.0.0.0', 8080 , app)
